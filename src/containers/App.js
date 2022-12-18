96b1cdac95fa7f673843c65b200f820eceb72c39
CHZelaya <chzelaya1201@gmail.com>
2022-12-17 21:59:51 -0700

    #1 added actions/reducers/constants.js and installed and imported react-redux and redux toolkit -- set up redux store, provider and connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+import { setSearchField, } from '../actions';
+
+const mapStateToProps = state => {
+  // this is the state of the redux store
+  // the state.searchRobots is the reducer's name
+  // the searchField is the property we want to get from the reducer
+  return {
+    searchField: state.searchField
+  }
+}
+
+
+
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => {
+      // dispatch the 'setSearchField' action
+      // when the search field changes
+      dispatch(setSearchField(event.target.value))
+    }
+  }
+}
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       robots: [],
-      searchfield: ''
     }
   }
 
@@ -19,21 +43,19 @@ class App extends Component {
       .then(users => { this.setState({ robots: users }) });
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
 
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return !robots.length ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
@@ -42,4 +64,6 @@ class App extends Component {
   }
 }
 
-export default App;
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
-import { createRoot } from "react-dom/client";
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './containers/App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+// import { configureStore } from '@reduxjs/toolkit';
+import { createStore } from '@reduxjs/toolkit';
+import { searchRobots } from './reducers';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(searchRobots);
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
